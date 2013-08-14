771df525af0a5f574f28e673141fc6eb8edefbd1
Georgi Denchev Ivanov <georgi.denchew@gmail.com>
2013-08-14 13:04:36 +0300

    gallery.data and gallery.repositories

---
+++ ImageGalleryProject/Gallery.Data/GalleryContext.cs
@@ -0,0 +1,26 @@
+﻿using Gallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Data
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext() :
+            base("GalleryDatabase")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ ImageGalleryProject/Gallery.Repositories/UnitOfWork.cs
@@ -0,0 +1,95 @@
+﻿using Gallery.Data;
+using Gallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Repositories
+{
+    public class UnitOfWork : IDisposable
+    {
+        private GalleryContext context = new GalleryContext();
+        private EfRepository<User> usersRepository;
+        private EfRepository<Album> albumsRepository;
+        private EfRepository<Image> imagesRepository;
+        private EfRepository<Comment> commentsRepository;
+        bool disposed;
+
+        public EfRepository<User> UsersRepository
+        {
+            get
+            {
+                if (this.usersRepository == null)
+                {
+                    this.usersRepository = new EfRepository<User>(context);
+                }
+
+                return this.usersRepository;
+            }
+        }
+
+        public EfRepository<Album> AlbumsRepository
+        {
+            get
+            {
+                if (this.albumsRepository == null)
+                {
+                    this.albumsRepository = new EfRepository<Album>(context);
+                }
+
+                return this.albumsRepository;
+            }
+        }
+
+        public EfRepository<Image> ImagesRepository
+        {
+            get
+            {
+                if (this.imagesRepository == null)
+                {
+                    this.imagesRepository = new EfRepository<Image>(context);
+                }
+
+                return this.imagesRepository;
+            }
+        }
+
+        public EfRepository<Comment> CommentsRepository
+        {
+            get
+            {
+                if (this.commentsRepository == null)
+                {
+                    this.commentsRepository = new EfRepository<Comment>(context);
+                }
+
+                return this.commentsRepository;
+            }
+        }
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+    }
+}
