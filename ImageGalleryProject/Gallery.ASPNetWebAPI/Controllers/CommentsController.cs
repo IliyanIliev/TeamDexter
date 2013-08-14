29576fae1e6fb014e9eb2255d6a601a504a52c6d
skolchev <stefan.t.kolchev@gmail.com>
2013-08-14 17:43:11 +0300

    added comment model

---
+++ ImageGalleryProject/Gallery.ASPNetWebAPI/Controllers/CommentsController.cs
@@ -0,0 +1,33 @@
+﻿using Gallery.ASPNetWebAPI.Models;
+using Gallery.Models;
+using Gallery.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Gallery.ASPNetWebAPI.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        private UnitOfWork unitOfWork;
+
+        public CommentsController()
+        {
+            this.unitOfWork = new UnitOfWork();
+        }
+
+        public CommentsController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<CommentModel> GetAll()
+        {
+            return this.unitOfWork.CommentsRepository.All()
+                .Select(CommentModel.FromComment).ToList();
+        }
+    }
+}
