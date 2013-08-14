09af5a038aee35be65ecdd9be53804721ca41896
skolchev <stefan.t.kolchev@gmail.com>
2013-08-14 18:21:25 +0300

    added web client

---
+++ ImageGalleryProject/Gallery.ASPNetWebAPI/Models/CommentFullModel.cs
@@ -0,0 +1,14 @@
+﻿using Gallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gallery.ASPNetWebAPI.Models
+{
+    public class CommentFullModel : CommentModel
+    {
+        public User Author { get; set; }
+
+    }
+}
