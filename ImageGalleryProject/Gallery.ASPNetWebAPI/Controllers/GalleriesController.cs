331c7a761ec1a4618cf49b0e4c7a59464212ce1a
skolchev <stefan.t.kolchev@gmail.com>
2013-08-15 20:27:45 +0300

    very important bug fix

--- ImageGalleryProject/Gallery.ASPNetWebAPI/Controllers/GalleriesController.cs
+++ ImageGalleryProject/Gallery.ASPNetWebAPI/Controllers/GalleriesController.cs
@@ -14,55 +14,6 @@ namespace Gallery.ASPNetWebAPI.Controllers
 {
     public class GalleriesController : BaseApiController
     {
-        //private UnitOfWork unitOfWork;
-
-        //public GalleriesController()
-        //{
-        //    this.unitOfWork = new UnitOfWork();
-        //}
-
-        //public GalleriesController(UnitOfWork unitOfWork)
-        //{
-        //    this.unitOfWork = unitOfWork;
-        //}
-
-        //public IEnumerable<Gallery.Models.Gallery> GetAll()
-        //{
-        //    return this.unitOfWork.GalleriesRepository.All().ToList();
-        //}
-
-        //public Gallery.Models.Gallery Get(int Id)
-        //{
-        //    return this.unitOfWork.GalleriesRepository.Get(Id);
-        //}
-
-        //public HttpResponseMessage Post([FromBody] Gallery.Models.Gallery gallery)
-        //{
-        //    this.unitOfWork.GalleriesRepository.Add(gallery);
-
-        //    HttpResponseMessage message = this.Request.CreateResponse(HttpStatusCode.Created);
-        //    message.Headers.Location =
-        //        new Uri(this.Request.RequestUri + gallery.ID.ToString(CultureInfo.InvariantCulture));
-
-        //    return message;
-        //}
-
-        //public HttpResponseMessage Put(int Id, Gallery.Models.Gallery gallery)
-        //{
-        //    this.unitOfWork.GalleriesRepository.Update(Id, gallery);
-
-        //    HttpResponseMessage message = this.Request.CreateResponse(HttpStatusCode.OK);
-        //    message.Headers.Location =
-        //        new Uri(this.Request.RequestUri + gallery.ID.ToString(CultureInfo.InvariantCulture));
-
-        //    return message;
-        //}
-
-        //public void Delete(int Id)
-        //{
-        //    this.unitOfWork.GalleriesRepository.Delete(Id);
-        //}
-
         [HttpGet]
         [ActionName("get")]
         public HttpResponseMessage GetGalleries(string sessionKey)
